91b4a6d020c3b59630369e7b7c2da8d065dd0d48
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-07-08 13:37:59 +0200

    prevent selection on nav bar

--- src/components/WorkSpace.vue
+++ src/components/WorkSpace.vue
@@ -89,6 +89,7 @@ section.workspace {
     display: flex;
     align-items: center;
     justify-content: flex-start;
+    user-select: none;
     & > div {
       height: 100%;
       padding-inline: 0.5rem;
